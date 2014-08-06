82f237a78203525e1961ab0676ef6569e3580704
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2014-08-06 17:12:49 +0100

    cosmetic

--- lib/exabgp/reactor/network/connection.py
+++ lib/exabgp/reactor/network/connection.py
@@ -133,7 +133,6 @@ class Connection (object):
 						self.logger.wire(LazyFormat("%s %-32s RECEIVED " % (self.name(),'%s / %s' % (self.local,self.peer)),od,read))
 						yield data
 						return
-					
 					yield ''
 			except socket.timeout,e:
 				self.close()
